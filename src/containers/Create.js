469f88031b658146ae8f58cc19cb547b7b428750
haoxiaoli <haoxiaoli@baidu.com>
2020-10-21 21:06:20 +0800

    update a item

--- src/containers/Create.js
+++ src/containers/Create.js
@@ -18,9 +18,11 @@ const tabsText = [LIST_VIEW, CHART_VIEW];
 class Create extends React.Component {
     constructor(props) {
         super(props);
+        const {id} = props.match.params;
+        const {categories, items} = props.data;
         this.state = {
-            selectedTab: TYPE_OUTCOME,
-            selectedCategory: null
+            selectedTab: (id && items[id]) ? categories[items[id].cid].type : TYPE_OUTCOME,
+            selectedCategory: (id && items[id]) ? categories[items[id].cid] : null
         }
     }
     tabChange = (index) => {
@@ -40,6 +42,7 @@ class Create extends React.Component {
             this.props.actions.createItem(data, this.state.selectedCategory.id);
         } else {
             // update
+            this.props.actions.updateItem(data, this.state.selectedCategory.id);
         }
         this.props.history.push('/');
     }
@@ -49,22 +52,27 @@ class Create extends React.Component {
     render() {
         const {data} = this.props;
         const {items, categories} = data;
-        const {selectedTab} = this.state;
+        const {id} = this.props.match.params;
+        const editItem = (id && items[id]) ? items[id] : {};
+        const {selectedTab, selectedCategory} = this.state;
         const filterCategories = Object.keys(categories)
         .filter(id => categories[id].type === selectedTab)
-        .map(id => categories[id])
+        .map(id => categories[id]);
+        const tabIndex = tabsText.findIndex(text => text === selectedTab);
         return (
             <div className="create-page py-3 px-3 rounded mt-3" style={{background: '#fff'}}>
-                <Tabs activeIndex={0} onTabChange={this.tabChange}>
+                <Tabs activeIndex={tabIndex} onTabChange={this.tabChange}>
                     <Tab>支出</Tab>
                     <Tab>收入</Tab>
                 </Tabs>
                 <CategorySelect
                     categories={filterCategories}
-                    onSelectCategory={this.selectCategory}/>
+                    onSelectCategory={this.selectCategory}
+                    selectedCategory={selectedCategory}/>
                 <PriceForm
                     onFormSubmit={this.submibForm}
                     onCancelSubmit={this.cancelSubmit}
+                    item={editItem}
                 />
             </div>
         );
